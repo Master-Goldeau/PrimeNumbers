74fdfcd613ddf700080380145af86855b460e17b
Master-Goldeau <34507362+Master-Goldeau@users.noreply.github.com>
2018-07-29 01:01:37 +0200

    Second Commit. An easier way with only two lists.
    Now Improve it using a real Iterator.

--- src/NombresPremiers/Initializer.java
+++ src/NombresPremiers/Initializer.java
@@ -2,97 +2,94 @@ package NombresPremiers;
 
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.ConsoleHandler;
 
 /**
  * Classe permettant l'initialisation du programme
+ * 
  * @author Etienne
  * 
  */
 
 public class Initializer {
 
-	private static ArrayList<ArrayList<Object>> firstNumbers = new ArrayList<ArrayList<Object>>();
+	private ArrayList<Integer> primeNumbers = new ArrayList<Integer>();
+	private ArrayList<Integer> numbersToCheck = new ArrayList<>();
 
-	private ArrayList<Object> listeVide = new ArrayList<>();
-	private ArrayList<Object> firstColumn = new ArrayList<>();
-	private ArrayList<Object> secondColumn = new ArrayList<>();
-	private ArrayList<Object> thirdColumn = new ArrayList<>();
-	private ArrayList<Object> fifthColumn = new ArrayList<>();
-
-	//Getters
-	
-	public ArrayList<ArrayList<Object>> getFirstNumbers() {
-		return firstNumbers;
+	// Getters
+	public ArrayList<Integer> getPrimeNumbers() {
+		return primeNumbers;
 	}
 
-	public ArrayList<Object> getListeVide() {
-		return listeVide;
+	public ArrayList<Integer> getNumbersToCheck() {
+		return numbersToCheck;
 	}
 
-	public ArrayList<Object> getFirstColumn() {
-		return firstColumn;
-	}
-
-	public ArrayList<Object> getSecondColumn() {
-		return secondColumn;
-	}
-
-	public ArrayList<Object> getThirdColumn() {
-		return thirdColumn;
-	}
-
-	public ArrayList<Object> getFifthColumn() {
-		return fifthColumn;
-	}
-
-	
-	
 	/**
-	 * Méthode di'initialisation des listes.
+	 * Méthode d'initialisation des listes.
+	 * 
 	 * @param aucun.
 	 */
-	
-	public void initLists() {
-		firstNumbers.add(0, listeVide);
-		firstNumbers.add(1, firstColumn);
-		firstNumbers.add(2, secondColumn);
-		firstNumbers.add(3, thirdColumn);
-		firstNumbers.add(4, listeVide);
-		firstNumbers.add(5, fifthColumn);
 
-		firstColumn.add(1);
-		secondColumn.add(2);
-		thirdColumn.add(3);
-		fifthColumn.add(5);
+	public void initLists() {
+
+		primeNumbers.add(2);
+		primeNumbers.add(3);
+		primeNumbers.add(5);
+
+	}
+
+	public void createListOfNumbersToCheck() {
+
+		int numberToAdd;
+
+		for (numberToAdd = 7; numberToAdd < 395;) {
+
+			numbersToCheck.add(numberToAdd);
+			numberToAdd = numberToAdd + 4;
+			numbersToCheck.add(numberToAdd);
+			numberToAdd = numberToAdd + 2;
+		}
+
 	}
 
 	/**
 	 * Méthode permettant de vérifier qu'un nombre est premier.
+	 * 
 	 * @param Integer.
-	 * @return false or true.
-	 * if return true, le nombre testé est inséré dans la première ou dans la 5ème colonne
-	 * en fonction de son résultat %6.
+	 * @return false or true. if return true, le nombre testé est inséré dans la
+	 *         première ou dans la 5ème colonne en fonction de son résultat %6.
 	 */
-	
+
+	/*
+	 * superList.add(new ArrayList<Object>()); superList.get(0).add(new Object());
+	 * // retourne un objet de type List<Object> superList.get(0).get(0); //
+	 * retourne un objet du type Object
+	 */
+
 	public boolean isPrime(int checkMe) {
-		
+
+		// Iterator<Object> iterator = firstColumn.iterator();
+
 		if (checkMe % 2 == 0)
 			return false;
-		for (int i = 2; i*i <= checkMe; i++) {
+		for (int i = 2; i * i <= checkMe; i++) {
 			if (checkMe % i == 0)
 				return false;
 		}
-		
-		switch (checkMe%6)
-		{
-		case 1: firstColumn.add(checkMe);
-		break;
-		case 5: fifthColumn.add(checkMe);
-		break;
-		default: System.out.println("ERROR");
+
+		switch (checkMe % 6) {
+		case 1:
+			primeNumbers.add(checkMe);
+			break;
+		case 5:
+			primeNumbers.add(checkMe);
+			break;
+		default:
+			System.out.println("ERROR");
 		}
-		
+
 		return true;
 	}
 
--- src/NombresPremiers/MainTest.java
+++ src/NombresPremiers/MainTest.java
@@ -4,32 +4,37 @@ import java.util.ArrayList;
 
 /**
  * Classe de démarrage de l'application
+ * 
  * @author Etienne
  *
  */
 public class MainTest {
-			
+
 	/*
-	 * superList.add(new ArrayList<Object>());
-	 * superList.get(0).add(new Object()); // retourne un objet de type List<Object>
-	 * superList.get(0).get(0); // retourne un objet du type Object			
+	 * superList.add(new ArrayList<Object>()); superList.get(0).add(new Object());
+	 * // retourne un objet de type List<Object> superList.get(0).get(0); //
+	 * retourne un objet du type Object
 	 */
-		
+
 	public static void main(String[] args) {
-		
+
 		Initializer init = new Initializer();
 		init.initLists();
+		init.createListOfNumbersToCheck();
+
+		int iterator=0;
 		
-		int checkMe;
-		
-		for(checkMe=7; checkMe<=251; checkMe++) {
-			init.isPrime(checkMe);
+		for (init.getNumbersToCheck().get(iterator); init.getNumbersToCheck().get(iterator) <= 251; iterator++) {
+			init.isPrime(init.getNumbersToCheck().get(iterator));
 		}
-		
-		System.out.println(init.getFirstNumbers().toString());
-//		System.out.println(firstNumbers.get(0).get(0).toString());
-//		System.out.println();
-//		System.out.println();
+
+		System.out.println(iterator);
+		System.out.println(init.getNumbersToCheck().get(iterator));
+		System.out.println(init.getNumbersToCheck().toString());
+		System.out.println(init.getPrimeNumbers().toString());
+		// System.out.println(firstNumbers.get(0).get(0).toString());
+		// System.out.println();
+		// System.out.println();
 	}
 
 }
